193fb56e02c4984ad2e7200abe5624974f666c69
Brian Westphal <brian.westphal@panchaininc.com>
2023-10-11 09:15:05 +0800

    v1.11.2: - Upgraded client-run-queue and react-bindings to latest versions - Upgraded dev dependencies to latest versions - Removed semvar resolution since previous security advisory (https://www.npmjs.com/advisories/1092310) has been resolved on all dependencies

--- src/waitable/utils.ts
+++ src/waitable/utils.ts
@@ -6,5 +6,5 @@ export const isWaitable = (value: any): value is Waitable<any, any> =>
   value !== null && typeof value === 'object' && 'isWaitable' in value && value.isWaitable === true;
 
 /** Returns a readonly waitable if the specified value is a waitable.  Otherwise, returns undefined */
-export const ifWaitable = <SuccessT, FailureT>(value: Waitable<SuccessT, FailureT> | any): Waitable<SuccessT, FailureT> | undefined =>
+export const ifWaitable = <SuccessT, FailureT>(value: any): Waitable<SuccessT, FailureT> | undefined =>
   isWaitable(value) ? (value as Waitable<SuccessT, FailureT>) : undefined;
